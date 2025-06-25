ea0f97e7ad08198ff2775d909f72c16176364cb0
not-log <56871060+not-log@users.noreply.github.com>
2025-06-25 20:17:44 +0400

    upd root

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,5 @@ if [ ! -f /config/settings.json ]; then
   cp /defaults/settings.json /config/
 fi
 
-# Установка прав
-# chown -R 1000:1000 /config /database /srv
-
 # ENTRYPOINT из https://github.com/filebrowser/filebrowser/blob/master/Dockerfile
 exec tini -- /init.sh filebrowser --config /config/settings.json
