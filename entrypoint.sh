1b336d9907e9964fcd877acefc24d0dbb2ecbbcf
not-log <56871060+not-log@users.noreply.github.com>
2025-06-25 22:59:37 +0400

    /srv/files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ if [ ! -f /config/settings.json ]; then
   cp /defaults/settings.json /config/
 fi
 
-# chown -R 1000:1000 /config /database /srv
+mkdir /srv/files
+chown -R 1000:1000 /config /database /srv/files
 
 # ENTRYPOINT из https://github.com/filebrowser/filebrowser/blob/master/Dockerfile
 exec tini -- /init.sh filebrowser --config /config/settings.json
