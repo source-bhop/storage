f8745cda174706c73cec7329935e35f504d9ba75
not-log <56871060+not-log@users.noreply.github.com>
2025-06-25 22:53:57 +0400

    проверка volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -f /config/settings.json ]; then
   cp /defaults/settings.json /config/
 fi
 
-chown -R 1000:1000 /config /database /srv
+# chown -R 1000:1000 /config /database /srv
 
 # ENTRYPOINT из https://github.com/filebrowser/filebrowser/blob/master/Dockerfile
 exec tini -- /init.sh filebrowser --config /config/settings.json
