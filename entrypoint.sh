a1b1fda9df2b0e8a4764d08b97caa663ece4ec6b
not-log <56871060+not-log@users.noreply.github.com>
2025-06-25 20:43:31 +0400

    правки

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,5 @@ if [ ! -f /config/settings.json ]; then
   cp /defaults/settings.json /config/
 fi
 
-chown -R 1000:1000 /config /database /srv
-
 # ENTRYPOINT из https://github.com/filebrowser/filebrowser/blob/master/Dockerfile
-exec su-exec 1000:1000 tini -- /init.sh filebrowser --config /config/settings.json
+exec tini -- /init.sh filebrowser --config /config/settings.json
